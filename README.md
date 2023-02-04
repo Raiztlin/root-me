2b3b47926b43c9a096c53a78bd8d96b7c8e5026a
Lenin <billy91larsson@gmail.com>
2023-02-04 06:23:45 +0100

    First

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# root-me
+# This is a repository for scripts I've written when doing mainly reverse engineering challenges found on root-me.org.
---
+++ basic_keygen.py
@@ -0,0 +1,15 @@
+from sys import stdin
+from hashlib import sha256
+
+userInput = stdin.read().strip()
+length = len(userInput)
+
+
+serialList =  [ord(userInput[i]) - i + 20 for i in range(length)]
+serial = bytearray(serialList)
+
+#with open(".m.key", "wb") as binary_file:
+#    binary_file.write(serial)
+
+print(sha256(serial).hexdigest())
+
